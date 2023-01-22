098cd6e6283ad1dd8e3aa8adf9030ac0ff1cb096
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2023-01-22 18:55:09 +0100

    add instrumentation test

---
+++ app/src/androidTest/java/com/example/tiptime/TipUITests.kt
@@ -0,0 +1,39 @@
+package com.example.tiptime
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.tiptime.ui.theme.TipTimeTheme
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.text.NumberFormat
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+               TipTimeScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)")
+            .performTextInput("20")
+        composeTestRule.onNodeWithText("Tip amount: ${NumberFormat.getCurrencyInstance().format(2.0)}")
+            .assertExists()
+    }
+}
